7dfe7e608ad3e05c613be600e25e52bd28611f65
Siwat Pruksapanya <siwat.pru@outlook.com>
2018-07-15 13:07:50 +0700

    :sparkles: Add gesture prediction

--- index.js
+++ index.js
@@ -1,8 +1,88 @@
 const app = require('express')();
 
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
+
+let data = {};
+let states = {};
+let model;
+let evaluate = true;
+let evaluating = false;
+
+tf.loadModel('file://./model.tfjs/model.json').then(x => {
+  console.log('Model loaded');
+  model = x;
+});
+
+function applyFilter(name, newMove) {
+  if (!states[name] || states[name].lastMove != newMove) {
+    states[name] = {
+      lastMove: newMove,
+      lastTime: Date.now(),
+    };
+    return 0;
+  } else {
+    var time = Date.now() - states[name].lastTime;
+    if (time > 100) {
+      return time;
+    } else {
+      return 0;
+    }
+  }
+}
+
+function parseData(arr) {
+  // Same logic as in python
+  var prevAccel = null;
+  var prevGyro = null;
+  var prevType = '';
+  var data = [];
+
+  var i = 0;
+  arr.forEach(current => {
+    if (current.type == 'gyro') prevGyro = current;
+    if (prevType == 'accel' && prevGyro) {
+      i++;
+      if (i % 2 == 1)
+        data.push([
+          prevAccel.x,
+          prevAccel.y,
+          prevAccel.z,
+          prevGyro.x,
+          prevGyro.y,
+          prevGyro.z,
+        ]);
+    }
+    prevType = current.type;
+    if (current.type == 'accel') prevAccel = current;
+  });
+
+  return data;
+}
+
+function clean(str) {
+  return str.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '-');
+}
+
+function updateDevices() {
+  if (controlSocket) {
+    controlSocket.emit('devices', connectedDevices.map(x => x.name));
+  }
+}
+
+function argMax(arr) {
+  var max = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[max]) {
+      max = i;
+    }
+  }
+  return max;
+}
 
 const STATE = {
   IDLE: 0,
@@ -84,6 +164,10 @@ const printAction = action => {
 };
 
 const processAction = (roomId, id, action) => {
+  if (!room[roomId]) {
+    return
+  }
+
   // Process player's state
   if (
     room[roomId].player1.id === id &&
@@ -170,14 +254,14 @@ const processAction = (roomId, id, action) => {
       room[roomId].player1.state,
     )} HP: ${room[roomId].player1.hp} Charge: ${
       room[roomId].player1.charge
-    } lastAction ${room[roomId].player1.lastAction}`,
+    } lastAction ${printAction(room[roomId].player1.lastAction)}`,
   );
   console.log(
     `${room[roomId].player2.id}: state: ${printState(
       room[roomId].player2.state,
     )} HP: ${room[roomId].player2.hp} Charge: ${
       room[roomId].player2.charge
-    } lastAction ${room[roomId].player2.lastAction}`,
+    } lastAction ${printAction(room[roomId].player2.lastAction)}`,
   );
 
   io.emit('game_state', room);
@@ -185,20 +269,20 @@ const processAction = (roomId, id, action) => {
   // TODO:Check Player's HP and update game state
   if (game.player1.hp <= 0) {
     console.log('Player2 win');
-    io.emit('game_end', {roomId: roomId, playerWin: 2})
-    delete room[roomId]
+    io.emit('game_end', {roomId: roomId, playerWin: 2});
+    delete room[roomId];
   }
   if (game.player2.hp <= 0) {
     console.log('Player1 win');
-    io.emit('game_end', {roomId: roomId, playerWin: 1})
-    delete room[roomId]
+    io.emit('game_end', {roomId: roomId, playerWin: 1});
+    delete room[roomId];
   }
 };
 
 io.on('connection', socket => {
-  console.log('Player connected to server');
+  console.log(`${socket.id} connected to server`);
 
-  socket.on('join_room', roomId => {
+  socket.on('join', roomId => {
     console.log(`${socket.id} joins ${roomId}`);
     if (!room[roomId]) {
       room[roomId] = Object.assign({}, game);
@@ -208,7 +292,38 @@ io.on('connection', socket => {
       room[roomId].player2.id = socket.id;
       io.emit('player_join', {roomId: roomId, playerNumber: 2});
       // Broadcast game started
-      io.emit('game_start', {roomId: roomId});
+      io.emit('game_start', {room: room});
+    }
+  });
+
+  socket.on('data', msg => {
+    const {roomId} = msg;
+
+    if (!data[socket.id]) data[socket.id] = [];
+    let thisData = data[socket.id];
+
+    thisData.push(msg);
+
+    if (evaluate && !evaluating && thisData.length > 50) {
+      evaluating = true;
+      const start = Date.now();
+      const parsedData = [parseData(thisData.slice(-100))];
+      let result = model.predict(tf.tensor3d(parsedData));
+      result.data().then(data => {
+        evaluating = false;
+        const max = argMax(data);
+        if (data[max] >= 0.5) {
+          const time = Date.now() - start;
+          const ms = applyFilter(socket.id, max)
+          if (ms) {
+            processAction(roomId, socket.id, max);
+          }
+        }
+      });
+    }
+    // Periodically clear
+    if (thisData.length > 5000) {
+      data[socket.id] = thisData.slice(-500);
     }
   });
 
